9e9241cb3414b5b035370dc082b86c6502586b86
Youssef Laaroussi <yousseflaar17@gmail.com>
2025-08-20 19:13:45 +0100

    adding fils

--- spring-security/src/main/java/com/youssef/spring/security/dto/RegisterRequest.java
+++ spring-security/src/main/java/com/youssef/spring/security/dto/RegisterRequest.java
@@ -1,13 +1,20 @@
 package com.youssef.spring.security.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
 
-        @NotBlank   String firstName,
-        @NotBlank   String lastName,
-        @NotBlank  String email,
-        @NotBlank @Size(min = 6) String password
+        @NotBlank
+        String firstName,
+        @NotBlank
+        String lastName,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Size(min = 6)
+        String password
 
 ) { }
